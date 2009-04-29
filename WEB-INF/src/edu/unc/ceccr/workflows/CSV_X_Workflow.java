5a1c2e39de473d4b731489850cb0585b726c07f5
Myroslav Sypa <msypa@email.unc.edu>
2009-04-29 15:28:58 +0000

    Scripts  DEBUG

--- WEB-INF/src/edu/unc/ceccr/workflows/CSV_X_Workflow.java
+++ WEB-INF/src/edu/unc/ceccr/workflows/CSV_X_Workflow.java
@@ -12,11 +12,13 @@ public class CSV_X_Workflow {
 	public CSV_X_Workflow(String userName, String datasetName, String sdfName) throws IOException{
 		this.viz_path = Constants.CECCR_USER_BASE_PATH+userName+"/DATASETS/" +datasetName+"/Visualization/"+sdfName;
 		this.file_path = Constants.CECCR_USER_BASE_PATH+userName+"/DATASETS/" +datasetName+"/"+sdfName;
+		
 	}
 	
 	public void performMACCSCreation(){
 		try{
 			Process p= Runtime.getRuntime().exec("moebatch_shell_script.sh "+file_path+".sdf "+viz_path+".maccs");
+			Utility.writeToMSDebug("Shell script: "+"moebatch_shell_script.sh "+file_path+".sdf "+viz_path+".maccs");
 			Utility.writeProgramLogfile(file_path, "moebatch_shell_script",  p.getInputStream(), p.getErrorStream());
 			p.waitFor();
 		}catch(Exception ex){
@@ -29,6 +31,7 @@ public class CSV_X_Workflow {
 		
 		try{
 			Process p = Runtime.getRuntime().exec("convert_maccs_to_X2.pl "+viz_path+".maccs "+viz_path+".x");
+			Utility.writeToMSDebug("X2 script: "+"convert_maccs_to_X2.pl "+viz_path+".maccs "+viz_path+".x");
 			Utility.writeProgramLogfile(viz_path, "convert_maccs_to_X2",  p.getInputStream(), p.getErrorStream());
 			p.waitFor();
 		}catch(Exception ex){
@@ -41,6 +44,7 @@ public class CSV_X_Workflow {
 		
 		try{
 			Process p = Runtime.getRuntime().exec("convert_x_to_csv.pl "+viz_path+".x "+viz_path+".csv");
+			Utility.writeToMSDebug("CVS script: "+"convert_x_to_csv.pl "+viz_path+".x "+viz_path+".csv");
 			Utility.writeProgramLogfile(viz_path, "convert_x_to_csv",  p.getInputStream(), p.getErrorStream());
 			p.waitFor();
 		}catch(Exception ex){
