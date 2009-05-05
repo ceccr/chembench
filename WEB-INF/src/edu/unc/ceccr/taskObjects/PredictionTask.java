e2fe1db3f76145f657b8a7ad7f0ed57b83a6efc9
Myroslav Sypa <msypa@email.unc.edu>
2009-05-05 21:17:13 +0000

    FEATURE: Dataset delete due to new tables in DB

--- WEB-INF/src/edu/unc/ceccr/taskObjects/PredictionTask.java
+++ WEB-INF/src/edu/unc/ceccr/taskObjects/PredictionTask.java
@@ -13,9 +13,10 @@ import org.hibernate.Transaction;
 import edu.unc.ceccr.persistence.HibernateUtil;
 import edu.unc.ceccr.utilities.Utility;
 
+@SuppressWarnings("serial")
 @Entity()
 @Table(name = "cbench_prediction_task")
-public class PredictionTask {
+public class PredictionTask implements java.io.Serializable{
 
 	private Long id = null;
 	
@@ -60,7 +61,7 @@ public class PredictionTask {
 		Transaction tx = null;
 		try {
 			tx = s.beginTransaction();
-			s.saveOrUpdate(t);
+			s.save(t);
 			tx.commit();
 		} catch (RuntimeException e) {
 			if (tx != null)
