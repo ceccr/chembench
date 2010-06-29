a1d73be81a4e9fcea2aa9222bf9073cacde1c7ba
lamt <lamt@lamt.com>
2010-06-29 18:20:19 +0000

    test removing 2 last lines in training X file and external X file

--- WEB-INF/src/edu/unc/ceccr/workflows/RandomForestWorkflow.java
+++ WEB-INF/src/edu/unc/ceccr/workflows/RandomForestWorkflow.java
@@ -15,14 +15,39 @@ import java.util.Scanner;
 public class RandomForestWorkflow{
 
 	public static void buildRandomForestModels(RandomForestParameters randomForestParameters, String actFileDataType, String scalingType, String workingDir) throws Exception{
+//		String trainingXFile = workingDir + Constants.MODELING_SET_X_FILE;
+//		String externalXFile = workingDir + Constants.EXTERNAL_SET_X_FILE;
+		String command = "";
 		Utility.writeToDebug("Running Random Forest Modeling...");
-		if(scalingType.equals(Constants.NOSCALING)){
-			//the last two lines of the .x file do not need to be removed
-			
-		}
-		else{
+//		Utility.writeToDebug("scalingType: " + scalingType);
+//		Utility.writeToDebug("Constants.NOSCALING: " + Constants.NOSCALING);
+		if(!scalingType.equals(Constants.NOSCALING)){
 			//the last two lines of the .x file need to be removed
+			/*
+			Utility.writeToDebug("Removing last 2 lines from " + trainingXFile);
+			command = "sed 'N;$!P;$!D;$d' < " + trainingXFile + " > " + trainingXFile + ".tmp : mv " + trainingXFile + ".tmp " + trainingXFile;
+			Utility.writeToDebug("Running external program: " + command + " in dir " + workingDir);
+			Process p = Runtime.getRuntime().exec(command, null, new File(workingDir));
+			p.waitFor();
 			
+			Utility.writeToDebug("Removing last 2 lines from " + externalXFile);
+			command = "sed 'N;$!P;$!D;$d' < " + trainingXFile + " > " + trainingXFile + ".tmp : mv " + trainingXFile + ".tmp " + trainingXFile;
+			Utility.writeToDebug("Running external program: " + command + " in dir " + workingDir);
+			Process p = Runtime.getRuntime().exec(command, null, new File(workingDir));
+			p.waitFor();
+			*/
+			
+			Utility.writeToDebug("Removing last 2 lines from " + Constants.MODELING_SET_X_FILE);
+			command = "sed 'N;$!P;$!D;$d' < " + Constants.MODELING_SET_X_FILE + " > " + Constants.MODELING_SET_X_FILE + ".tmp : mv " + Constants.MODELING_SET_X_FILE + ".tmp " + Constants.MODELING_SET_X_FILE;
+			Utility.writeToDebug("Running external program: " + command + " in dir " + workingDir);
+			Process p = Runtime.getRuntime().exec(command, null, new File(workingDir));
+			p.waitFor();
+			
+			Utility.writeToDebug("Removing last 2 lines from " + Constants.EXTERNAL_SET_X_FILE);
+			command = "sed 'N;$!P;$!D;$d' < " + Constants.EXTERNAL_SET_X_FILE + " > " + Constants.EXTERNAL_SET_X_FILE + ".tmp : mv " + Constants.EXTERNAL_SET_X_FILE + ".tmp " + Constants.EXTERNAL_SET_X_FILE;
+			Utility.writeToDebug("Running external program: " + command + " in dir " + workingDir);
+			p = Runtime.getRuntime().exec(command, null, new File(workingDir));
+			p.waitFor();
 		}
 	}
 
