15e3411d740dfe17c4631e9818d686d4f9805354
Myroslav Sypa <msypa@email.unc.edu>
2011-11-18 22:20:45 +0000

    CLEANUP: Cleaned unused imports

--- WEB-INF/src/edu/unc/ceccr/action/CeccrBaseAction.java
+++ WEB-INF/src/edu/unc/ceccr/action/CeccrBaseAction.java
@@ -1,35 +1,14 @@
 package edu.unc.ceccr.action;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+
 
 //struts2
 import com.opensymphony.xwork2.ActionSupport; 
 import com.opensymphony.xwork2.ActionContext; 
 
-import org.apache.struts.upload.FormFile;
-import org.apache.struts2.interceptor.SessionAware;
-import org.hibernate.Session;
 
-import edu.unc.ceccr.global.Constants;
-import edu.unc.ceccr.jobs.CentralDogma;
-import edu.unc.ceccr.persistence.DataSet;
-import edu.unc.ceccr.persistence.HibernateUtil;
-import edu.unc.ceccr.persistence.Job;
-import edu.unc.ceccr.persistence.Prediction;
-import edu.unc.ceccr.persistence.Predictor;
 import edu.unc.ceccr.persistence.User;
-import edu.unc.ceccr.taskObjects.QsarModelingTask;
-import edu.unc.ceccr.utilities.PopulateDataObjects;
 import edu.unc.ceccr.utilities.Utility;
 
 public class CeccrBaseAction extends ActionSupport {
