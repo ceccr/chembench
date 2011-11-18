332c1d27d51b4583ed0042d1251da1ab2b663556
Myroslav Sypa <msypa@email.unc.edu>
2011-11-18 22:32:08 +0000

    FEATURE: Delete guest on session timeout

--- WEB-INF/src/edu/unc/ceccr/utilities/ActiveUser.java
+++ WEB-INF/src/edu/unc/ceccr/utilities/ActiveUser.java
@@ -3,6 +3,9 @@ package edu.unc.ceccr.utilities;
 import javax.servlet.http.HttpSessionListener;
 import javax.servlet.http.HttpSessionEvent;
 
+import edu.unc.ceccr.action.HomeAction;
+import edu.unc.ceccr.persistence.User;
+
 public class ActiveUser implements HttpSessionListener{
 	private static int activeSessions=0;
 	
@@ -17,8 +20,19 @@ public class ActiveUser implements HttpSessionListener{
 	}
 	public void sessionDestroyed(HttpSessionEvent se)
 	{
-		if(activeSessions>0)
+		
+		if(activeSessions>0 && se!=null){
 			activeSessions--;
+			if(se.getSession()!=null && se.getSession().getAttribute("userType")!=null){
+				User user = (User)se.getSession().getAttribute("user");
+				Utility.writeToDebug("Session destruction begins for user "+user.getUserName());
+				String type = (String)se.getSession().getAttribute("userType");
+				if(user!=null && user.getUserName()!=null && user.getUserName().contains("guest") && type!=null && type.equals("guest")){
+					new HomeAction().deleteGuest(user);
+					Utility.writeToDebug("GUEST USER DELETED on SESSION TIMEOUT:"+user.getUserName());
+				}
+			}
+		}
 	}
 	
 }
