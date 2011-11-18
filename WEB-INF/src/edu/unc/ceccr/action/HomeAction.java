2c13c0f3d501b1ca62d5329c07a83875a8f4b5b5
Myroslav Sypa <msypa@email.unc.edu>
2011-11-18 22:21:54 +0000

    FEATURE: Added functions for new Guest architecture

--- WEB-INF/src/edu/unc/ceccr/action/HomeAction.java
+++ WEB-INF/src/edu/unc/ceccr/action/HomeAction.java
@@ -3,19 +3,23 @@ package edu.unc.ceccr.action;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 
 //struts2
+import com.mysql.jdbc.Util;
 import com.opensymphony.xwork2.ActionSupport; 
 import com.opensymphony.xwork2.ActionContext; 
 
 import edu.unc.ceccr.global.Constants;
 import edu.unc.ceccr.jobs.CentralDogma;
 import edu.unc.ceccr.persistence.DataSet;
+import edu.unc.ceccr.persistence.ExternalValidation;
 import edu.unc.ceccr.persistence.HibernateUtil;
 import edu.unc.ceccr.persistence.Job;
 import edu.unc.ceccr.persistence.JobStats;
@@ -152,43 +156,129 @@ public class HomeAction extends ActionSupport implements ServletResponseAware {
 		if(context.getParameters().get("username") != null){
 			username = ((String[]) context.getParameters().get("username"))[0];
 		}
+		User user;
 		
-		Session s = HibernateUtil.getSession();
-		User user = PopulateDataObjects.getUserByUserName(username, s);
-		s.close();
-		
-		if(user != null){
-			String realPasswordHash = user.getPassword();
+		if(context.getParameters().get("ip") != null){
 			
-			if (password != null && Utility.encrypt(password).equals(realPasswordHash)){
-				context.getSession().put("user", user);
-				Cookie ckie=new Cookie("login","true");
-				servletResponse.addCookie(ckie);
+			String ip =  ((String[]) context.getParameters().get("ip"))[0];
+			long time = System.currentTimeMillis();
+			String new_username = "guest"+ip+"_"+time;
+			
+			user = new User();
+
+			user.setUserName(new_username);
+			user.setEmail("ceccr@email.unc.edu");
+			user.setFirstName("Guest");
+			user.setLastName("Guest");
+			user.setOrgName("Guest");
+			user.setOrgType("Academia");
+			user.setOrgPosition("Guest");
+			user.setPhone("Guest");
+			user.setAddress("Guest");
+			user.setState("Guest");
+			user.setCity("Guest");
+			user.setCountry("Guest");
+			user.setZipCode("Guest");
+			
+			//options
+			user.setShowPublicDatasets(Constants.SOME);
+			user.setShowPublicPredictors(Constants.ALL);
+			user.setViewDatasetCompoundsPerPage(Constants.TWENTYFIVE);
+			user.setViewPredictionCompoundsPerPage(Constants.TWENTYFIVE);
+			user.setShowAdvancedKnnModeling(Constants.NO);
+			
+			//rights
+			user.setCanDownloadDescriptors(Constants.NO);
+			user.setIsAdmin(Constants.NO);
+			
+			//password 
+			String password = "";
+			user.setPassword(Utility.encrypt(password));
+			
+			user.setStatus("agree");
 				
-				Utility.writeToUsageLog("Logged in", user.getUserName());
+			Session s = HibernateUtil.getSession();
+			Transaction tx = null;
+		
+			if(s==null){
+				loginFailed = Constants.YES;
+				return ERROR;
 			}
-			else if(user.getUserName().equals("guest")){
-				Utility.writeToUsageLog("Logged in", user.getUserName());
-				context.getSession().put("user", user);
-				Cookie ckie=new Cookie("login","true");
-				servletResponse.addCookie(ckie);
-				new Thread(new Runnable() {
-					
-					@Override
-					public void run() {
-						clearOldGuestData();
-						
-					}
-				}).start();
+			//commit user to DB
+			
+			try {
+				tx = s.beginTransaction();
+				s.saveOrUpdate(user);
+				tx.commit();
+			} catch (RuntimeException e) {
+				if (tx != null)
+					tx.rollback();
+					loginFailed = Constants.YES;
+				Utility.writeToDebug(e);
+			} finally {
+				s.close();
+			}
+			
+			new Thread(new Runnable() {
 				
-				Utility.writeToUsageLog("Logged in", user.getUserName());
+				@Override
+				public void run() {
+					
+					deleteOldGuests();
+					
+				}
+			}).start();
+			
+			context.getSession().put("user", user);
+			context.getSession().put("userType", "guest");
+			Cookie ckie = new Cookie("login","true");
+			servletResponse.addCookie(ckie);
+				
+			Utility.writeToUsageLog("Logged in guest::", user.getUserName());
+			
+		}
+		else{
+		
+			Session s = HibernateUtil.getSession();
+		
+			user = PopulateDataObjects.getUserByUserName(username, s);
+			s.close();
+		
+		
+			if(user != null){
+				String realPasswordHash = user.getPassword();
+				
+				if (password != null && Utility.encrypt(password).equals(realPasswordHash)){
+					context.getSession().put("user", user);
+					Cookie ckie=new Cookie("login","true");
+					servletResponse.addCookie(ckie);
+					user.setLastLogintime(new Date());
+					
+					s = HibernateUtil.getSession();
+					Transaction tx = null;
+
+					try {
+						tx = s.beginTransaction();
+						s.saveOrUpdate(user);
+						tx.commit();
+					} catch (RuntimeException e) {
+						if (tx != null)
+							tx.rollback();
+						Utility.writeToDebug(e);
+					} finally {
+						s.close();
+					}
+					
+					
+					Utility.writeToUsageLog("Logged in", user.getUserName());
+				}
+				else{
+					loginFailed = Constants.YES;
+				}
 			}
 			else{
 				loginFailed = Constants.YES;
 			}
-		}
-		else{
-			loginFailed = Constants.YES;
 		}
 		loadPage();
 		return result;
@@ -196,11 +286,18 @@ public class HomeAction extends ActionSupport implements ServletResponseAware {
 	
 	public String logout() throws Exception{
 		ActionContext context = ActionContext.getContext();
+		
 		user = (User) context.getSession().get("user");
+		
 		if(user != null){
 			Utility.writeToUsageLog("Logged out.", user.getUserName());
 		}
+		Utility.writeToDebug("************Logout action "+user.getUserName());
 		
+		if(user.getUserName().contains("guest") && context.getSession().get("userType")!=null && ((String)context.getSession().get("userType")).equals("guest")){
+			deleteGuest(user);
+		}
+		context.getSession().remove("user"); 
 		context.getSession().clear();
 		
 		Cookie ckie=new Cookie("login","false");
@@ -210,36 +307,25 @@ public class HomeAction extends ActionSupport implements ServletResponseAware {
 		return SUCCESS;
 	}
 	
-	public void clearOldGuestData(){
+	public boolean deleteGuest(User user){
 		try{
-			Date current_date = new Date();
-			Session session = HibernateUtil.getSession();
-			String guestPath = Constants.CECCR_USER_BASE_PATH+"workflow-users/guest/";
-			//getting all jobs for guest user
-			List<JobStats> jobStatList = PopulateDataObjects.getJobStatsByUserName(session, "guest");
-			List<JobStats> datasetJobs = new ArrayList<JobStats>();
-			List<JobStats> modelingJobs = new ArrayList<JobStats>();
-			List<JobStats> predictionJobs = new ArrayList<JobStats>();
-			//populating a list of all the jobs for user guest that are older than 3 hours
-			for(JobStats js: jobStatList){
-				if(js.getTimeFinished() != null && js.getTimeStarted() != null){
-					if(js.getTimeFinished().getTime() < current_date.getTime()-Constants.GUEST_DATA_EXPIRATION_TIME){
-						if(js.getJobType().equals("DATASET")) datasetJobs.add(js);
-						if(js.getJobType().equals("MODELING")) modelingJobs.add(js);
-						if(js.getJobType().equals("PREDICTION")) predictionJobs.add(js);
-					}
-					
-				}
-			}
 			
-			//get all predictions based on the previous data
-			for(JobStats js: predictionJobs){
-				Utility.writeToDebug("+++++++++++++++DELETE_PREDICTIONS++++++++++++++");
-				if(new File(guestPath+"PREDICTIONS/"+js.getJobName()).exists()) Utility.writeToDebug("DELETE:::"+guestPath+"PREDICTIONS/"+js.getJobName());//FileAndDirOperations.deleteDir(new File(guestPath+"PREDICTIONS/"+js.getJobName()));
-				Prediction p = PopulateDataObjects.getPredictionByName(js.getJobName(),"guest",session);
-				if(p!=null){
+			String userToDelete=user.getUserName();
+			if(!userToDelete.trim().isEmpty()){
+				Utility.writeToDebug("Delete GUEST");
+				Session s = HibernateUtil.getSession();
+				
+				ArrayList<Prediction> predictions = (ArrayList<Prediction>) PopulateDataObjects.getUserData(userToDelete, Prediction.class, s);
+				ArrayList<Predictor> predictors = (ArrayList<Predictor>) PopulateDataObjects.getUserData(userToDelete,Predictor.class, s);
+				ArrayList<DataSet> datasets = (ArrayList<DataSet>) PopulateDataObjects.getUserData(userToDelete,DataSet.class, s);
+				ArrayList<Job> jobs = (ArrayList<Job>) PopulateDataObjects.getUserData(userToDelete,Job.class, s);
+				
+				s.close();
+
+				for(Prediction p: predictions){
+					Session session = HibernateUtil.getSession();	
 					ArrayList<PredictionValue> pvs = (ArrayList<PredictionValue>) PopulateDataObjects.getPredictionValuesByPredictionId(p.getId(), session);
-					Utility.writeToDebug("DELETE_DB:::"+p.getName()+"......"+pvs.size());
+					
 					if(pvs != null){
 						for(PredictionValue pv : pvs){
 							Transaction tx = null;
@@ -253,63 +339,114 @@ public class HomeAction extends ActionSupport implements ServletResponseAware {
 									tx.rollback();
 								Utility.writeToDebug(e);
 							}
+							finally{
+								session.close();
+							}
 						}
 					}
-					
-					//delete the database entry for the prediction
+					Transaction tx = null;
+					try{
+						session = HibernateUtil.getSession();
+						tx = session.beginTransaction();
+						session.delete(p);
+						tx.commit();
+					}catch (RuntimeException e) {
+						Utility.writeToDebug(e);
+					}
+					finally{
+						session.close();
+					}
+				}
+
+				for(Predictor p: predictors){
+					Session session = HibernateUtil.getSession();
+					ArrayList<ExternalValidation> extVals = new ArrayList<ExternalValidation>();
+					ArrayList<Predictor> childPredictors = new ArrayList<Predictor>();
+					if(p.getChildIds() != null && ! p.getChildIds().trim().equals("")){
+						String[] childIdArray = p.getChildIds().split("\\s+");
+						for(String childId: childIdArray){
+							Predictor childPredictor = PopulateDataObjects.getPredictorById(Long.parseLong(childId), session);
+							childPredictors.add(childPredictor);
+							extVals.addAll(PopulateDataObjects.getExternalValidationValues(childPredictor.getId(), session));
+						}
+					}
+					extVals.addAll(PopulateDataObjects.getExternalValidationValues(p.getId(), session));
+					session.close();
+					session = HibernateUtil.getSession();
 					Transaction tx = null;
 					try{
 						tx = session.beginTransaction();
 					    session.delete(p);
+					    session.close();
+					    session = HibernateUtil.getSession();
+					    tx = session.beginTransaction();
+					    for(Predictor childPredictor: childPredictors){
+					    	session.delete(childPredictor);
+					    }
+					    for(ExternalValidation ev: extVals){
+					    	session.delete(ev);
+					    }
 						tx.commit();
 					}catch (RuntimeException e) {
-						if (tx != null)
-							tx.rollback();
 						Utility.writeToDebug(e);
 					}
-					session.close();
+					finally{
+						session.close();
+					}
 				}
-					
-			}
-			
-			//get all predictors based on the previous data
-			for(JobStats js: modelingJobs){
-				Utility.writeToDebug("+++++++++++++++DELETE_PREDICTORS++++++++++++++");
-				if(new File(guestPath+"PREDICTORS/"+js.getJobName()).exists()) Utility.writeToDebug("DELETE:::"+guestPath+"PREDICTORS/"+js.getJobName());//FileAndDirOperations.deleteDir(new File(guestPath+"PREDICTIONS/"+js.getJobName()));
-				Predictor p = PopulateDataObjects.getPredictorByName(js.getJobName(),"guest",session);
-				if(p!=null){
-					Utility.writeToDebug("DELETE_DB:::"+p.getName());
-					new DeleteAction().deletePredictor(p, session);
-				}
-	
-			}
-			
-			//get all datasets based on the previous data
-			for(JobStats js: datasetJobs){
-				Utility.writeToDebug("+++++++++++++++DELETE_DATASETS++++++++++++++");
-				if(new File(guestPath+"DATASETS/"+js.getJobName()).exists()) Utility.writeToDebug("DELETE:::"+guestPath+"DATASETS/"+js.getJobName());//FileAndDirOperations.deleteDir(new File(guestPath+"PREDICTIONS/"+js.getJobName()));
-				DataSet ds = PopulateDataObjects.getDataSetByName(js.getJobName(),"guest",session);
-				if(ds!=null){
-					Utility.writeToDebug("DELETE_DB:::"+ds.getName());
+
+				for(DataSet d: datasets){
+					Session session = HibernateUtil.getSession();	
 					Transaction tx = null;
-					try{
-						tx = session.beginTransaction();
-					    session.delete(ds);
-						tx.commit();
-					}catch (RuntimeException e) {
-						if (tx != null)
-							tx.rollback();
-						Utility.writeToDebug(e);
-					}
-	
-					session.close();
+					tx = session.beginTransaction();
+					session.delete(d);
+					tx.commit();
 				}
+
+				for(Job j: jobs){
+					CentralDogma.getInstance().localJobs.removeJob(j.getId());
+					Session session = HibernateUtil.getSession();	
+					Transaction tx = null;
+					tx = session.beginTransaction();
+					session.delete(j);
+					tx.commit();
+				}
+
+			    try {
+			    	Session session = HibernateUtil.getSession();	
+					Transaction tx = null;
+					tx = session.beginTransaction();
+					session.delete(user);
+					tx.commit();
+			    	
+			    }
+			    catch(Exception ex){
+			    	Utility.writeToDebug(ex);
+			    }
+			    
+			    //last, delete all the files that user has
+			    Utility.writeToDebug("Delete GUEST:::ALL DATA FROM DB SHOULD BE DELETED");
+				File dir= new File(Constants.CECCR_USER_BASE_PATH + userToDelete); 
+				boolean flag = FileAndDirOperations.deleteDir(dir);//recurses
+				Utility.writeToDebug("Delete GUEST:::ALL DATA FROM FILES SHOULD BE DELETED:"+flag);
 			}
-		
-			
 		}
-		catch(Exception ex){
-			Utility.writeToDebug(ex);
+		catch (Exception e) {
+			Utility.writeToDebug(e);
+			return false;
+		}
+		return true;
+	}
+	
+	synchronized public void deleteOldGuests(){
+		List<String> dirs= FileAndDirOperations.getGuestDirNames(new File(Constants.CECCR_USER_BASE_PATH));
+		long currentTime = System.currentTimeMillis();
+		for(String dir:dirs){
+			long guestTime =  new Long(dir.substring(dir.lastIndexOf('_')+1)).longValue();
+			if(!dir.trim().isEmpty() && currentTime-guestTime>Constants.GUEST_DATA_EXPIRATION_TIME){
+				FileAndDirOperations.deleteDir(new File(Constants.CECCR_USER_BASE_PATH+dir));
+				Utility.writeToDebug("DELETING OLD GUEST DATA:"+dir);
+			}
 		}
 	}
 	
