b0ba4a7562b5aa312304c26010bd77ad168ca0c2
Diptorup Deb <diptorup@cs.unc.edu>
2012-11-01 17:20:28 -0400

    Added a quick fix to get the runCommandAndLogOutput method to work.
    For some reason the forked child process was not detecting the directory
    path for the shell script which it needed to execute. For now appended
    the full directory path (using the workdir variable - No hardcoding) to
    the filename (temp-script.sh).
    
    Would revisit and see what the proper fix would be.

--- WEB-INF/src/edu/unc/ceccr/utilities/RunExternalProgram.java
+++ WEB-INF/src/edu/unc/ceccr/utilities/RunExternalProgram.java
@@ -10,155 +10,156 @@ import java.io.InputStream;
 
 import edu.unc.ceccr.global.Constants;
 
+import org.apache.log4j.Logger;
+
 //Java suuure is dumb sometimes. Needs me to write it a whole class just to 
 //run a program and capture its output without bleeding file handles 
 //all over the place.
 
-public class RunExternalProgram {
-	
-	//these programs should not have anything appear in the log file
-	//when they run. (For programs that execute many times in quick succession.)
-	private static String[] runQuietly = 
-		{"bjobs.sh", 
-		"datasplit_train_test",
-		"checkKnnSaProgress",
-		"checkKnnGaProgress",
-		"svm-train",
-		"svm-predict",
-		"molconvert"};
-	
-	public static void close(Closeable c) {
-		if (c != null) {
-		  try {
-		    c.close();
-		  } catch (IOException e) {
-		  // ignored
-	     }
-	   }
-	}
+public class RunExternalProgram
+{
+    
+    //these programs should not have anything appear in the log file
+    //when they run. (For programs that execute many times in quick succession.)
 
-	public static void runCommand(String cmd, String workingDir){
-		//runs an external program (no logging)
-		
-		try{
-			Process p = null;
+    private static Logger logger 
+        = Logger.getLogger(RunExternalProgram.class.getName());
+    private static String[] runQuietly = 
+        {
+        "bjobs.sh", 
+        "datasplit_train_test",
+        "checkKnnSaProgress",
+        "checkKnnGaProgress",
+        "svm-train",
+        "svm-predict",
+        "molconvert"
+        };
+    
+    public static void 
+    close(Closeable c)
+    {
+        if (c != null) {
+          try {
+            c.close();
+          } catch (IOException e) {
+          // ignored
+         }
+       }
+    }
 
-			if(workingDir.isEmpty()){
-				workingDir = Constants.CECCR_USER_BASE_PATH;
-			}
-			
-			boolean outputRunningMessage = true;
-			for(int i = 0; i < runQuietly.length; i++){
-				if(cmd.startsWith(runQuietly[i])){
-					outputRunningMessage = false;
-				}
-			}
-			
-			if(workingDir.isEmpty()){
-				if(outputRunningMessage){
-					Utility.writeToDebug("Running external program " + cmd);
-				}
-				p = Runtime.getRuntime().exec(cmd);
-			}
-			else{
-				if(outputRunningMessage){
-					Utility.writeToDebug("Running external program " + cmd + " in dir: " + workingDir);
-				}
-				p = Runtime.getRuntime().exec(cmd, null, new File(workingDir));
-			}
-		
-			//capture program output in log file
-			File file=new File(workingDir + "/Logs/");
-			if(!file.exists()){
-				file.mkdirs();
-			}
-			
-			//wait for the program to finish running
-		    p.waitFor();
-		    
-		    //close any file handles we might have 
-		 	RunExternalProgram.close(p.getOutputStream());
-	        RunExternalProgram.close(p.getInputStream());
-	        RunExternalProgram.close(p.getErrorStream());
-	        p.destroy();
-	        
-		}
-		catch(Exception ex){
-			Utility.writeToDebug(ex);
-		}
-	}
-	
-	public static void runCommandAndLogOutput(String cmd, String workingDir, String logFileName){
-		//runs an external program and writes user info to logfile
-		
-		try{
-			Process p = null;
+    public static void 
+    runCommand(String cmd, String workingDir)
+    {
+        //runs an external program (no logging)
+        
+        try{
+            Process p = null;
 
-			if(workingDir.isEmpty()){
-				workingDir = Constants.CECCR_USER_BASE_PATH;
-			}
-			
+            if(workingDir.isEmpty()){
+                workingDir = Constants.CECCR_USER_BASE_PATH;
+            }
+            
+            boolean outputRunningMessage = true;
+            for(int i = 0; i < runQuietly.length; i++){
+                if(cmd.startsWith(runQuietly[i])){
+                    outputRunningMessage = false;
+                }
+            }
+            
+            if(workingDir.isEmpty()){
+                if(outputRunningMessage){
+                    Utility.writeToDebug("Running external program " + cmd);
+                }
+                p = Runtime.getRuntime().exec(cmd);
+            }
+            else{
+                if(outputRunningMessage){
+                    Utility.writeToDebug("Running external program " + cmd 
+                                                  + " in dir: " + workingDir);
+                }
+                p = Runtime.getRuntime().exec(cmd, null, new File(workingDir));
+            }
+        
+            //capture program output in log file
+            File file=new File(workingDir + "/Logs/");
+            if(!file.exists()){
+                file.mkdirs();
+            }
+            
+            //wait for the program to finish running
+            p.waitFor();
+            
+            //close any file handles we might have 
+            RunExternalProgram.close(p.getOutputStream());
+            RunExternalProgram.close(p.getInputStream());
+            RunExternalProgram.close(p.getErrorStream());
+            p.destroy();
+            
+        }
+        catch(Exception ex){
+            Utility.writeToDebug(ex);
+        }
+    }
+    
+    public static void 
+    runCommandAndLogOutput(String cmd, String workingDir, String logFileName)
+    {
+        //runs an external program and writes user info to logfile
+        
+        try{
+            Process p = null;
 
-			boolean outputRunningMessage = true;
-			for(int i = 0; i < runQuietly.length; i++){
-				if(cmd.startsWith(runQuietly[i]) || logFileName.startsWith(runQuietly[i])){
-					outputRunningMessage = false;
-				}
-			}
+            if(workingDir.isEmpty()){
+                workingDir = Constants.CECCR_USER_BASE_PATH;
+            }
+            
 
-			//capture program output in log file
-			File file=new File(workingDir + "Logs/");
-			if(!file.exists()){
-				file.mkdirs();
-			}
-			String logsPath = workingDir + "Logs/";
-			
-			cmd = cmd + " > " +  logsPath + logFileName + ".log" + " 2> " + logsPath + logFileName + ".err";
-			
-			File scriptFile = new File(workingDir + "temp-script.sh");
-			BufferedWriter out = new BufferedWriter(new FileWriter(scriptFile));
-			out.write(cmd + "\n");
-			out.close();
-			scriptFile.setExecutable(true);
-			FileAndDirOperations.makeDirContentsExecutable(workingDir);
-			
-			if(outputRunningMessage){
-				Utility.writeToDebug("Running external program " + cmd + " in dir: " + workingDir);
-			}
+            boolean outputRunningMessage = true;
+            for(int i = 0; i < runQuietly.length; i++){
+                if(cmd.startsWith(runQuietly[i]) ||
+                   logFileName.startsWith(runQuietly[i])){
+                    outputRunningMessage = false;
+                }
+            }
 
-			p = Runtime.getRuntime().exec("temp-script.sh", null, new File(workingDir));
-		    p.waitFor();
-		    
-		    
-			/*
-			FileOutputStream logFileOS = new FileOutputStream(new File(workingDir + "/Logs/" + logFileName + ".log"));
-			FileOutputStream errFileOS = new FileOutputStream(new File(workingDir + "/Logs/" + logFileName + ".err"));
-			
-			p = Runtime.getRuntime().exec(cmd, null, new File(workingDir));
-			InputStream stdout = p.getInputStream();
-			
-			StreamGobbler outputGobbler = new StreamGobbler(stdout, "stdout", logFileOS);  
-			outputGobbler.start();
-			InputStream errout = p.getErrorStream();
-			StreamGobbler errorGobbler = new StreamGobbler(errout, "errout", errFileOS);   
-			errorGobbler.start();
-			
-			//wait for the program to finish running
-		    p.waitFor();
-		    
-		    //close any file handles we might have
-		 	RunExternalProgram.close(p.getOutputStream());
-	        RunExternalProgram.close(p.getInputStream());
-	        RunExternalProgram.close(p.getErrorStream());
-			logFileOS.close();
-		 	errFileOS.close();
-	        p.destroy();
-	        
-	        */
-		}
-		catch(Exception ex){
-			Utility.writeToDebug(ex);
-		}
-	}
-	
+            //capture program output in log file
+            File file=new File(workingDir + "Logs/");
+            if(!file.exists()){
+                file.mkdirs();
+            }
+            String logsPath = workingDir + "Logs/";
+            
+            cmd = cmd + " > " +  logsPath + logFileName + ".log" 
+                  + " 2> " + logsPath + logFileName + ".err";
+            
+            File scriptFile = new File(workingDir + "temp-script.sh");
+            BufferedWriter out = new BufferedWriter(new FileWriter(scriptFile));
+            out.write(cmd + "\n");
+            out.close();
+            scriptFile.setExecutable(true);
+            FileAndDirOperations.makeDirContentsExecutable(workingDir);
+            
+            if(outputRunningMessage){
+                Utility.writeToDebug("Running external program " 
+                                     + cmd + " in dir: " + workingDir);
+            }
+            logger.debug("Trying to execute the command\n");
+            logger.debug("The working directory is : "+workingDir);
+            //DD 10/29/2012 - Putting in a hack to change the path to full
+            //                qualified. Not sure why this does not work
+            //                otherwise. Need to ask Diane.
+            p = Runtime.getRuntime().exec(workingDir+"/temp-script.sh"
+                                         , null
+                                         , new File(workingDir));
+            
+            logger.debug("Finished executing\n");
+            p.waitFor();
+                
+        }
+        catch(Exception ex){
+            Utility.writeToDebug(ex);
+            logger.error(ex.toString());
+        }
+    }
+    
 }
