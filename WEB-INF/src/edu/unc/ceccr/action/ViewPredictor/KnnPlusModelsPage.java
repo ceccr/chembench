530b4f441f57947ffc6ac69f4e7a30cbcd29f5fa
Ian Kim <iansjk@gmail.com>
2013-05-20 10:00:45 -0400

    Fixed formatting in KnnPlusModelsPage action
    Caused by committing from server to fix model-view bug.
    Reapplied Eclipse formatter according to Diptorup's spec.

--- WEB-INF/src/edu/unc/ceccr/action/ViewPredictor/KnnPlusModelsPage.java
+++ WEB-INF/src/edu/unc/ceccr/action/ViewPredictor/KnnPlusModelsPage.java
@@ -17,12 +17,9 @@ import edu.unc.ceccr.utilities.PopulateDataObjects;
 
 public class KnnPlusModelsPage extends ViewPredictorAction
 {
-
-    /**
-     * 
-     */
-    private static final long serialVersionUID = 1L;
-    private static Logger logger = Logger.getLogger(KnnPlusModelsPage.class.getName());
+    private static final long  serialVersionUID = 1L;
+    private static Logger      logger           = Logger.getLogger(KnnPlusModelsPage.class
+                                                        .getName());
     private List<KnnPlusModel> knnPlusModels;
 
     public String load() throws Exception
@@ -39,7 +36,7 @@ public class KnnPlusModelsPage extends ViewPredictorAction
             }
             else {
                 currentFoldNumber = ""
-                    + (Integer.parseInt(currentFoldNumber) + 1);
+                        + (Integer.parseInt(currentFoldNumber) + 1);
                 for (int i = 0; i < childPredictors.size(); i++) {
                     foldNums.add("" + (i + 1));
                     if (currentFoldNumber.equals("" + (i + 1))) {
@@ -52,32 +49,31 @@ public class KnnPlusModelsPage extends ViewPredictorAction
             }
 
             // get descriptor freqs from models
-            HashMap<String, Integer> descriptorFreqMap 
-                = new HashMap<String, Integer>();
+            HashMap<String, Integer> descriptorFreqMap = new HashMap<String, Integer>();
             if (knnPlusModels != null) {
                 for (KnnPlusModel m : knnPlusModels) {
-                    if (m.getDimsNames() != null && !m.getDimsNames().equals("")) {
-                        String[] descriptorArray = m.getDimsNames().split("\\s+");
+                    if (m.getDimsNames() != null
+                            && !m.getDimsNames().equals("")) {
+                        String[] descriptorArray = m.getDimsNames().split(
+                                "\\s+");
                         for (int i = 0; i < descriptorArray.length; i++) {
-                            if (descriptorFreqMap.get(descriptorArray[i]) == null){
+                            if (descriptorFreqMap.get(descriptorArray[i]) == null) {
                                 descriptorFreqMap.put(descriptorArray[i], 1);
                             }
                             else {
                                 // increment
-                                descriptorFreqMap
-                                    .put(descriptorArray[i],
-                                            descriptorFreqMap
-                                            .get(descriptorArray[i]) + 1);
+                                descriptorFreqMap.put(descriptorArray[i],
+                                        descriptorFreqMap
+                                                .get(descriptorArray[i]) + 1);
                             }
                         }
                     }
                 }
             }
 
-            ArrayList<descriptorFrequency> descriptorFrequencies 
-                = new ArrayList<descriptorFrequency>();
-            ArrayList<String> mapKeys = new ArrayList<String>(descriptorFreqMap
-                    .keySet());
+            ArrayList<descriptorFrequency> descriptorFrequencies = new ArrayList<descriptorFrequency>();
+            ArrayList<String> mapKeys = new ArrayList<String>(
+                    descriptorFreqMap.keySet());
             for (String k : mapKeys) {
                 descriptorFrequency df = new descriptorFrequency();
                 df.setDescriptor(k);
@@ -88,32 +84,31 @@ public class KnnPlusModelsPage extends ViewPredictorAction
             Collections.sort(descriptorFrequencies,
                     new Comparator<descriptorFrequency>()
                     {
-                    public int compare(descriptorFrequency df1,
-                        descriptorFrequency df2)
-                    {
-                    if (df1.getNumOccs() == df2.getNumOccs()) {
-                    return 0;
-                    }
-                    else if (df1.getNumOccs() > df2.getNumOccs()) {
-                    return -1;
-                    }
-                    else {
-                    return 1;
-                    }
-                    //return (df1.getNumOccs() > df2.getNumOccs() ? -1 : 1);
-                    }
+                        public int compare(descriptorFrequency df1,
+                                           descriptorFrequency df2)
+                        {
+                            if (df1.getNumOccs() == df2.getNumOccs()) {
+                                return 0;
+                            }
+                            else if (df1.getNumOccs() > df2.getNumOccs()) {
+                                return -1;
+                            }
+                            else {
+                                return 1;
+                            }
+                        }
                     });
             if (descriptorFrequencies.size() >= 5) {
-                // if there weren't at least 5 descriptors, don't even bother - no
-                // summary needed
-                mostFrequentDescriptors = "The 5 most frequent descriptors " +
-                    "                             used in your models were: ";
+                // if there weren't at least 5 descriptors, don't even bother
+                // - no summary needed
+                mostFrequentDescriptors = "The 5 most frequent descriptors "
+                        + "                             used in your models were: ";
                 for (int i = 0; i < 5; i++) {
                     mostFrequentDescriptors += descriptorFrequencies.get(i)
-                        .getDescriptor()
-                        + " ("
-                        + descriptorFrequencies.get(i).getNumOccs()
-                        + " models)";
+                            .getDescriptor()
+                            + " ("
+                            + descriptorFrequencies.get(i).getNumOccs()
+                            + " models)";
                     if (i < 4) {
                         mostFrequentDescriptors += ", ";
                     }
@@ -137,8 +132,8 @@ public class KnnPlusModelsPage extends ViewPredictorAction
             knnPlusModels = new ArrayList<KnnPlusModel>();
             session = HibernateUtil.getSession();
             List<KnnPlusModel> temp = PopulateDataObjects
-                .getKnnPlusModelsByPredictorId(Long.parseLong(objectId),
-                        session);
+                    .getKnnPlusModelsByPredictorId(Long.parseLong(objectId),
+                            session);
             session.close();
             if (temp != null) {
                 Iterator<KnnPlusModel> it = temp.iterator();
@@ -164,18 +159,18 @@ public class KnnPlusModelsPage extends ViewPredictorAction
     }
 
     @SuppressWarnings("unused")
-        private String loadModelSets()
-        {
-            String result = SUCCESS;
-            for (Predictor childPredictor : childPredictors) {
-                objectId = "" + childPredictor.getId();
-                result = loadModels();
-                if (!result.equals(SUCCESS)) {
-                    return result;
-                }
+    private String loadModelSets()
+    {
+        String result = SUCCESS;
+        for (Predictor childPredictor : childPredictors) {
+            objectId = "" + childPredictor.getId();
+            result = loadModels();
+            if (!result.equals(SUCCESS)) {
+                return result;
             }
-            return result;
         }
+        return result;
+    }
 
     public List<KnnPlusModel> getKnnPlusModels()
     {
