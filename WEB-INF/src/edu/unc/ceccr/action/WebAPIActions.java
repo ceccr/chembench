8789177da93a2f80d4e11870b7a8be659aa8b6fa
Ian Kim <iansjk@gmail.com>
2013-09-08 13:38:11 -0400

    Added error message support

--- WEB-INF/src/edu/unc/ceccr/action/WebAPIActions.java
+++ WEB-INF/src/edu/unc/ceccr/action/WebAPIActions.java
@@ -4,6 +4,9 @@ import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 import org.apache.log4j.Logger;
 
+import java.util.List;
+import java.util.ArrayList;
+
 /**
  * Actions for handling Web API requests.
  *
@@ -14,9 +17,11 @@ public class WebAPIActions extends ActionSupport {
     private static final long serialVersionUID = 1L;
     private static Logger logger = Logger.getLogger(
             WebAPIActions.class.getName());
+    private List<String> errorStrings = new ArrayList<String>();
 
     public String generatePredictor() {
         // NYI
+        errorStrings.add("This method has not been implemented yet.");
         return ERROR;
     }
 
@@ -33,6 +38,14 @@ public class WebAPIActions extends ActionSupport {
     private String convertSmilesToSdf(String[] smiles) {
         // NYI
         return null;
+    }
+
+    // --- begin getters / setters ---
+    public List<String> getErrorStrings() {
+        return errorStrings;
+    }
+    public void setErrorStrings(List<String> errorStrings) {
+        this.errorStrings = errorStrings;
     }
 }
 
