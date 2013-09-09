586ddc7b3d148c38ed03cb94ae1da95d48069820
Ian Kim <iansjk@gmail.com>
2013-09-09 00:22:30 -0400

    Wrote a routine to produce input file for molconvert

--- WEB-INF/src/edu/unc/ceccr/action/WebAPIActions.java
+++ WEB-INF/src/edu/unc/ceccr/action/WebAPIActions.java
@@ -4,8 +4,13 @@ import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 import org.apache.log4j.Logger;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Map;
 
 /**
  * Actions for handling Web API requests.
@@ -76,6 +81,19 @@ public class WebAPIActions extends ActionSupport {
                         i, names[i], smiles[i], activities[i]));
         }
 
+        // convert smiles strings into sdf file
+        String tempSdfFile;
+        try {
+            tempSdfFile = convertSmilesToSdf(names, smiles);
+        } catch (IOException e) {
+            errorStrings.add("SMILES to SDF conversion failed: " +
+                             e.getMessage());
+            return ERROR;
+        } catch (RuntimeException e) {
+            errorStrings.add(e.getMessage());
+            return ERROR;
+        }
+
         // NYI
         errorStrings.add("This method has not been implemented yet.");
         return ERROR;
@@ -92,8 +110,44 @@ public class WebAPIActions extends ActionSupport {
      * @return
      *      A path to the generated SDF file.
      */
-    private String convertSmilesToSdf(String[] names, String[] smiles) {
-        return null;
+    private String convertSmilesToSdf(String[] names, String[] smiles)
+            throws IOException {
+        assert names.length == smiles.length;
+
+        // create a temporary file and write out the names + smiles to it
+        File tempFile = File.createTempFile("smiles", ".tmp");
+        String tempFileLocation = tempFile.getAbsolutePath();
+        FileWriter fw = new FileWriter(tempFile);
+        BufferedWriter out = new BufferedWriter(fw);
+        try {
+            for (int i = 0; i < names.length; i++) {
+                if (smiles[i].isEmpty()) {
+                    // clean up and exit
+                    out.close();
+                    tempFile.delete();
+                    throw new RuntimeException(String.format(
+                            "Compound %d has an empty SMILES string.", i + 1));
+                }
+                if (names[i].isEmpty()) {
+                    out.close();
+                    tempFile.delete();
+                    throw new RuntimeException(String.format(
+                            "Compound %d has an empty compound name.", i + 1));
+                }
+                out.write(smiles[i]);
+                out.write(" ");
+                out.write(names[i]);
+
+                logger.debug(String.format("Wrote line %d: \"%s %s\", file=%s",
+                        i + 1, smiles[i], names[i], tempFileLocation));
+            }
+        } catch (IOException e) {
+            logger.error(e);
+            throw e;
+        } finally {
+            out.close();
+        }
+        return tempFileLocation;
     }
 
     // --- begin getters / setters ---
