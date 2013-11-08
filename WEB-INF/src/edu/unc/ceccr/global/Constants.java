422b0ff5c6ab336a61d09c34c0cc637a5c4d95d7
Ian Kim <iansjk@gmail.com>
2013-11-08 12:54:12 -0500

    Fixed up whitespace
    Removed some dangling whitespace and standardized line endings to LF.

--- WEB-INF/src/edu/unc/ceccr/global/Constants.java
+++ WEB-INF/src/edu/unc/ceccr/global/Constants.java
@@ -4,42 +4,42 @@ import java.util.ArrayList;
 
 public class Constants {
 
-	public static boolean doneReadingConfigFile=false; 
+	public static boolean doneReadingConfigFile=false;
 	public static int SESSION_EXPIRATION_TIME = 21600;
 	    //How long a login will last if idle, measured in seconds. 21600 seconds = 6 hours.
-	
+
 	//After submitting around 250 jobs, LSF won't let you submit more, it just returns an error.
 	//Cap the number of jobs that can be added to the LSF queue at any given time.
-	public static final int MAXLSFJOBS = 200; 
-	
+	public static final int MAXLSFJOBS = 200;
+
 	//We have 36 slots in the Emerald "patrons" queue, which is better than
-	//all the other queues (month, week, and idle). 
+	//all the other queues (month, week, and idle).
 	//Note that other lab members in the group "ceccr_grp" can also submit patrons
-	//jobs; use bjobs -u ceccr_grp to see if they have done so. 
+	//jobs; use bjobs -u ceccr_grp to see if they have done so.
 	//18 slots are left open for other lab members. (Just to be nice.)
 	//So until 18 jobs (submitted by anyone) are running in "patrons", all jobs are submitted there.
-	public static final int PATRONSQUEUESLOTS = 18; 
-	
+	public static final int PATRONSQUEUESLOTS = 18;
+
 	public static final int REPORTED_SIGNIFICANT_FIGURES = 4;
-	
+
 	//Enums for Data Types
-	
+
 	//Descriptor constants
 	public static final int NUM_MACCS_KEYS = 400;
 		//In practice, there are only 2 different types of MACCS keys -- 166 and 320.
 		//We use the 166 version. Padding doesn't hurt, though -- the extra 0's get removed later.
 	public static final int MOLCONNZ_FORMULA_POS = 11;
 	public static final int MOLCONNZ_COMPOUND_NAME_POS = 10;
-	
+
 	//file paths
 	public static String LSFJOBPATH;
 	public static String USERWORKFLOWSPATH;
-	
+
 	//Type strings
-	//The point of having these is to define capitalizations 
+	//The point of having these is to define capitalizations
 	//(prevents case-sensitivity bugs)
 	//and to define all possible settings for various objects.
-	
+
 	public static final String CONTINUOUS = "CONTINUOUS";
 	public static final String CATEGORY = "CATEGORY";
 
@@ -47,13 +47,13 @@ public class Constants {
 	public static final String LSF = "LSF";
 	public static final String LOCAL = "LOCAL";
 	public static final String ERROR = "ERROR";
-	
+
 	public static final String QUEUED = "QUEUED";
 	public static final String PREPROC = "PREPROC";
 	public static final String RUNNING = "RUNNING";
 	public static final String POSTPROC = "POSTPROC";
-	public static final String LSFWAIT = "LSFWAIT"; 
-	
+	public static final String LSFWAIT = "LSFWAIT";
+
 	public static final String NONE = "NONE";
 	public static final String SOME = "SOME";
 	public static final String TOP10 = "TOP10";
@@ -74,7 +74,7 @@ public class Constants {
 	public static final String TOXICITY = "Toxicity";
 	public static final String DRUGDISCOVERY = "DrugDiscovery";
 
-	public static final String DATASET = "DATASET"; 
+	public static final String DATASET = "DATASET";
 	public static final String MODELING = "MODELING";
 	public static final String PREDICTION = "PREDICTION";
 	public static final String MODELINGWITHDESCRIPTORS = "MODELINGWITHDESCRIPTORS";
@@ -82,7 +82,7 @@ public class Constants {
 
 	public static final String INVERSEOFSIZE = "INVERSEOFSIZE";
 	public static final String NOWEIGHTING = "NOWEIGHTING";
-	
+
 	public static final String MOLCONNZ = "MOLCONNZ";
 	public static final String DRAGONH = "DRAGONH";
 	public static final String DRAGONNOH = "DRAGONNOH";
@@ -98,10 +98,10 @@ public class Constants {
 	public static final String KNNPLUS = "KNNPLUS";
 	public static final String SVM = "SVM";
 	public static final String RANDOMFOREST = "RANDOMFOREST";
-	
+
 	public static final String SIMULATEDANNEALING = "SIMULATEDANNEALING";
 	public static final String GENETICALGORITHM = "GENETICALGORITHM";
-	
+
 	public static final String AUTOSCALING = "AUTOSCALING";
 	public static final String RANGESCALING = "RANGESCALING";
 	public static final String NOSCALING = "NOSCALING";
@@ -110,13 +110,13 @@ public class Constants {
 	public static final String SPHEREEXCLUSION = "SPHEREEXCLUSION";
 	public static final String USERDEFINED = "USERDEFINED";
 	public static final String NFOLD = "NFOLD";
-	
+
 	//steps in dataset task
 	public static final String SETUP = "Waiting in queue";
 	public static final String VISUALIZATION = "Generating visualizations";
 	public static final String SKETCHES = "Generating compound sketches";
 	public static final String SPLITDATA = "Splitting Data";
-	
+
 	//steps in modeling task
 	//also uses SETUP and SPLITDATA
 	public static final String STANDARDIZING = "Standardizing compounds";
@@ -127,13 +127,13 @@ public class Constants {
 	public static final String MODELS = "Generating models";
 	public static final String PREDEXT = "Predicting external set";
 	public static final String READING = "Reading output files";
-	
+
 	//steps in prediction task
 	//also uses DESCRIPTORS and SETUP
 	public static final String COPYPREDICTOR = "Copying predictor";
 	public static final String PREDICTING = "Predicting dataset";
 	public static final String READPRED = "Reading predictions";
-	
+
 	//kNN Constants
 	public static final int CONTINUOUS_NNN_LOCATION = 1;
 	public static final int CONTINUOUS_Q_SQUARED_LOCATION = 2;
@@ -144,7 +144,7 @@ public class Constants {
 	public static final int CONTINUOUS_K2_LOCATION = 15;
 	public static final int CONTINUOUS_R01_SQUARED_LOCATION = 16;
 	public static final int CONTINUOUS_R02_SQUARED_LOCATION = 17;
-	
+
 	//kNN Constants
 	//nnn, Training Accuracy, Normalized Training Accuracy, Test Accuracy, and Normalized Test Accuracy.
 	public static final int CATEGORY_NNN_LOCATION = 2;
@@ -155,28 +155,28 @@ public class Constants {
 
 	//External Validation Constants
 	public static final int COMPOUND_ID = 0;
-	    //Following constants decreased by 1. Wonder what this will do... 
-	public static final int STRUCTURE_FILE = 0; 
+	    //Following constants decreased by 1. Wonder what this will do...
+	public static final int STRUCTURE_FILE = 0;
 	public static final int ACTUAL = 1;
 	public static final int PREDICTED = 2;
 	public static final int NUM_MODELS = 3;
 	public static final int STD_DEVIATION = 3;
-	
+
 	//Paths
 	public static String CECCR_BASE_PATH;
 	public static String TOMCAT_PATH;
 	public static String BUILD_DATE_FILE_PATH;
 	public static String BUILD_DATE;
 	public static String CECCR_USER_BASE_PATH;
-	public static String XML_FILE_PATH; 
+	public static String XML_FILE_PATH;
 	public static String SDFILE_FILEPATH;
 	public static String DATAFILE_FILEPATH;
 	public static String IMAGE_FILEPATH;
 	public static String SYSTEMCONFIG_XML_PATH;
-	
+
 	public static String CATEGORY_DATAFILE_FILEPATH;
 	public static String CONTINUOUS_DATAFILE_FILEPATH;
-	
+
 	public static String EXECUTABLEFILE_PATH;
 	public static String INSTALLS_PATH;
 	public static String MOLCONNZ_MODELING_DATFILE_PATH;
@@ -197,15 +197,15 @@ public class Constants {
 	public static String SE_DEFAULT_FILENAME;
 	public static String DESCRIPTOR_ERROR_FILE;
 	public static String KNNPLUS_MODELS_FILENAME;
-	
+
 	public static String EXTERNAL_SET_A_FILE;
 	public static String EXTERNAL_SET_X_FILE;
 	public static String MODELING_SET_A_FILE;
 	public static String MODELING_SET_X_FILE;
-		
+
 	//for testing
 	public static final Integer MAX_FILE_SIZE = new Integer("1024");
-	
+
 	//administration
 	public static String WEBADDRESS;
 	public static String WEBSITEEMAIL;
@@ -213,36 +213,36 @@ public class Constants {
 	public static ArrayList<String> ADMIN_LIST=new ArrayList<String>();
 	public static ArrayList<String> ADMINEMAIL_LIST=new ArrayList<String>();
 	public static ArrayList<String> DESCRIPTOR_DOWNLOAD_USERS_LIST=new ArrayList<String>();
-	
+
 	// web service validation
 	public static  String RECAPTCHA_PUBLICKEY;
 	public static  String RECAPTCHA_PRIVATEKEY;
-	
+
 	public static int MAXCOMPOUNDS=200;
 	public static int MAXMODELS=10000;
-	
+
 	public static String DATABASE_URL;
     public static String DATABASE_DRIVER;
 	public static String DATABASE_USERNAME;
 	public static String CECCR_DATABASE_NAME;
 	public static String CECCR_DATABASE_PASSWORD;
-	
+
 	public static final String ALL_USERS_USERNAME = "all-users";
-	
+
 	public static String WORKBENCH;
 	public static final String CCHEMBENCH="cchembench";
 	public static final String CTOXBENCH="ctoxbench";
 	public static final String MAINKNN="MAINKNN";
 	public static final String RANDOMKNN="RANDOMKNN";
-	
+
 	//used by password hash function when user gets or changes their password
 	public static final String SOURCE="qwertyuio123NBV456pasdfghOPASDFGHjklm7890QWERTYUInbvcxzJKLMCXZ";
 	public static final String VALIDATOR_STRING="1234567890~!@#$%^&*()=+[]{}|:;'<>?";
-	
+
 	//a time in miliseconds the guest data is available, after that time will pass we'll delete guest data
 	//3 hours for now
 	public static final long GUEST_DATA_EXPIRATION_TIME = 10800000;
-	
+
 
     // the minimum number of compounds required for a dataset job.
     // users submitting a job with fewer than this number will have their job
