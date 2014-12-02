d16a3db4a6ee22c935c4c0ccf779d0dd4be5edb5
Ian Kim <iansjk@gmail.com>
2014-12-02 17:02:35 -0500

    Removed url_friendly_id

--- src/main/java/edu/unc/ceccr/chembench/persistence/Compound.java
+++ src/main/java/edu/unc/ceccr/chembench/persistence/Compound.java
@@ -8,7 +8,6 @@ public class Compound implements java.io.Serializable {
 
     private String compoundId;
     private String activityValue;
-    //private String url_friendly_id;
 
     public String getCompoundId() {
         return compoundId;
@@ -25,15 +24,4 @@ public class Compound implements java.io.Serializable {
     public void setActivityValue(String activityValue) {
         this.activityValue = activityValue;
     }
-
-    public String getUrl_friendly_id() {
-        return compoundId.replaceAll("%", "%25");
-    }
-    /*
-    public void setUrl_friendly_id(String url_friendly_id) {
-		this.url_friendly_id = url_friendly_id;
-	}
-	*/
-
-
 }
