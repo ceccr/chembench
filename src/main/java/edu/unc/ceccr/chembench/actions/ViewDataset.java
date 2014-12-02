7551a3ba81a8cecdd4748193e5228d2b40e6f74b
Ian Kim <iansjk@gmail.com>
2014-12-02 17:02:53 -0500

    (trivial) Formatting

--- src/main/java/edu/unc/ceccr/chembench/actions/ViewDataset.java
+++ src/main/java/edu/unc/ceccr/chembench/actions/ViewDataset.java
@@ -97,7 +97,8 @@ public class ViewDataset extends ActionSupport {
                 // load external compounds from file
                 externalCompounds = Lists.newArrayList();
                 HashMap<String, String> actIdsAndValues =
-                        DatasetFileOperations.getActFileIdsAndValues(datasetDirPath.resolve(Constants.EXTERNAL_SET_A_FILE)
+                        DatasetFileOperations.getActFileIdsAndValues(datasetDirPath.resolve(
+                                Constants.EXTERNAL_SET_A_FILE)
                                 .toString());
                 List<String> compoundIds = Lists.newArrayList(actIdsAndValues.keySet());
                 for (String compoundId : compoundIds) {
