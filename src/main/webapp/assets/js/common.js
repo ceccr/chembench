c4526907544967588344b9023d728a8733b5cb2d
Ian Kim <iansjk@gmail.com>
2014-12-08 22:46:11 -0500

    (trivial) Moved global into relevant portion

--- src/main/webapp/assets/js/common.js
+++ src/main/webapp/assets/js/common.js
@@ -1,8 +1,3 @@
-var guestMessage = "A guest account allows a user to explore the functionality of Chembench using publicly "
-        + "available datasets, predictions on single molecules, and modeling using Random Forests. "
-        + "<br><br> All guest data is deleted when you leave the site or become inactive for 90 minutes. "
-        + "For additional functionality, please register an account.";
-
 $(document).ready(function() {
     // navigation button handlers
     $(".nav-list li").mouseup(function(event) {
@@ -23,6 +18,12 @@ $(document).ready(function() {
     });
     $("#guest-login").click(function(event) {
         event.preventDefault();
+
+        var guestMessage = "A guest account allows a user to explore the functionality of Chembench using publicly "
+            + "available datasets, predictions on single molecules, and modeling using Random Forests. "
+            + "<br><br> All guest data is deleted when you leave the site or become inactive for 90 minutes. "
+            + "For additional functionality, please register an account.";
+
         bootbox.confirm(guestMessage, function(response) {
             if (response === true) {
                 window.location = $("#guest-login").attr("href");
