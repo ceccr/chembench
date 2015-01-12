96b5b3291b2a8836bad4f99bf4e7b23cee1bfbed
Ian Kim <iansjk@gmail.com>
2015-01-11 19:26:34 -0500

    Fixed up style reset code for dataset selection

--- src/main/webapp/assets/js/modeling.js
+++ src/main/webapp/assets/js/modeling.js
@@ -1,57 +1,35 @@
 function updateDatasetInfo(idString) {
     var id = parseInt(idString);
     var datasetInfo = $("#dataset-info");
-    var datasetInfoHelp = $("#dataset-info-help");
 
-    if (id === 0) {
-        datasetInfoHelp.show();
-        datasetInfo.hide();
-    } else if (id > 0) {
-        datasetInfoHelp.hide();
-        datasetInfo.show();
-        $.get("/ajaxGetDataset", {"id": id}, function (dataset) {
-            datasetInfo.html(
-                "<h4>Dataset: " + dataset.name + "</h4>" +
-                '<dl class="dl-horizontal properties-list">' +
-                "<dt>Number of compounds</dt>" +
-                "<dd>" + dataset.numCompound + "</dd>" +
-                "<dt>Activity type</dt>" +
-                "<dd>" + (dataset.continuous === true ? "Continuous" : "Category") + "</dd>" +
-                '<dt class="availableDescriptors">Available descriptors</dt>' +
-                '<dd class="available-descriptors">' + dataset.availableDescriptors + "</dd>" +
-                "</dl>"
-            );
+    $("#dataset-info-help").hide();
+    datasetInfo.show();
+    $.get("/ajaxGetDataset", {"id": id}, function(dataset) {
+        datasetInfo.html("<h4>Dataset: " + dataset.name + "</h4>" + '<dl class="dl-horizontal properties-list">' + "<dt>Number of compounds</dt>" + "<dd>" + dataset.numCompound + "</dd>" + "<dt>Activity type</dt>" + "<dd>" + (dataset.continuous === true ? "Continuous" : "Category") + "</dd>" + '<dt class="availableDescriptors">Available descriptors</dt>' + '<dd class="available-descriptors">' + dataset.availableDescriptors + "</dd>" + "</dl>");
 
-            var availableDescriptors = datasetInfo.find(".available-descriptors").text().trim().split(/\s+/);
-            if ($.inArray("UPLOADED", availableDescriptors) >= 0) {
-                datasetInfo.find("dl").append(
-                    "<dt>Uploaded descriptor type</dt>" +
-                    "<dd>" + dataset.uploadedDescriptorType + "</dd>"
-                );
-            }
-            // pre-select the default descriptor set, if available
-            var defaultDescriptor = $('input[type="hidden"]#defaultDescriptorGenerationType').val();
-            var defaultDescriptorOption = $('input[name="descriptorGenerationType"][value="' + defaultDescriptor + '"]');
-            if (!defaultDescriptorOption.prop("disabled")) {
-                defaultDescriptorOption.prop("checked", true);
-            }
+        var availableDescriptors = datasetInfo.find(".available-descriptors").text().trim().split(/\s+/);
+        if ($.inArray("UPLOADED", availableDescriptors) >= 0) {
+            datasetInfo.find("dl").append("<dt>Uploaded descriptor type</dt>" + "<dd>" + dataset.uploadedDescriptorType + "</dd>");
+        }
+        // enable only available descriptors in Descriptor Set selection
+        $('input[name="descriptorGenerationType"]').prop("disabled", true).removeAttr("checked").parent().addClass("text-muted");
+        for (var i = 0; i < availableDescriptors.length; i++) {
+            var availableDescriptorOption = $('input[name="descriptorGenerationType"][value="' + availableDescriptors[i] + '"]');
+            availableDescriptorOption.removeAttr("disabled").parent().removeClass("text-muted");
+        }
 
-            // enable only available descriptors in Descriptor Set selection
-            $('input[name="descriptorGenerationType"]').prop("disabled", true).parent().addClass("text-muted");
-            for (var i = 0; i < availableDescriptors.length; i++) {
-                var availableDescriptorOption = $('input[name="descriptorGenerationType"][value="' + availableDescriptors[i] + '"]');
-                availableDescriptorOption.removeAttr("disabled").parent().removeClass("text-muted");
-            }
+        // pre-select the default descriptor set, if available
+        var defaultDescriptor = $('input[type="hidden"]#defaultDescriptorGenerationType').val();
+        var defaultDescriptorOption = $('input[name="descriptorGenerationType"][value="' + defaultDescriptor + '"]');
+        if (!defaultDescriptorOption.prop("disabled")) {
+            defaultDescriptorOption.prop("checked", true);
+        }
 
-            // pretty-print the available descriptors list when we're done
-            formatAvailableDescriptors(datasetInfo.find(".available-descriptors"));
-        }).fail(function () {
-            datasetInfo.html(
-                '<h4 class="text-danger">Error fetching dataset info</h4>' +
-                "<p>A server error occurred while fetching dataset information for the selected dataset."
-            );
-        });
-    }
+        // pretty-print the available descriptors list when we're done
+        formatAvailableDescriptors(datasetInfo.find(".available-descriptors"));
+    }).fail(function() {
+        datasetInfo.html('<h4 class="text-danger">Error fetching dataset info</h4>' + "<p>A server error occurred while fetching dataset information for the selected dataset.");
+    });
 }
 
 $(document).ready(function() {
@@ -60,17 +38,22 @@ $(document).ready(function() {
     var descriptorTypes = $("#descriptor-types");
     descriptorTypes.find("label").addClass("text-muted").find("input").prop("disabled", true);
 
+    var datasetInfo = $("#dataset-info");
+    var datasetInfoHelp = $("#dataset-info-help");
     var datasetSelect = $("#dataset-selection");
     var viewDatasetDetailButton = $("#view-dataset-detail");
     datasetSelect.prepend('<option selected="selected" value="0">(Select a dataset)</option>');
     datasetSelect.change(function() {
         var datasetId = parseInt(this.value);
         if (datasetId === 0) {
+            datasetInfo.hide();
+            datasetInfoHelp.show();
+            descriptorTypes.find("label").addClass("text-muted").find("input").prop("disabled", true).prop("checked", false);
             viewDatasetDetailButton.addClass("disabled");
         } else {
             viewDatasetDetailButton.removeClass("disabled");
+            updateDatasetInfo(datasetId);
         }
-        updateDatasetInfo(datasetId);
     });
 
     viewDatasetDetailButton.click(function(e) {
