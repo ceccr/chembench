8ed1fc96de567d5cd497f9fa730dcb86f7fe8114
Ian Kim <iansjk@gmail.com>
2015-03-17 21:07:21 -0400

    Added JSON endpoints for My Bench items

---
+++ src/main/java/edu/unc/ceccr/chembench/actions/api/MyBenchAction.java
@@ -0,0 +1,56 @@
+package edu.unc.ceccr.chembench.actions.api;
+
+import com.google.common.collect.Lists;
+import com.opensymphony.xwork2.ActionSupport;
+import edu.unc.ceccr.chembench.global.Constants;
+import edu.unc.ceccr.chembench.persistence.Dataset;
+import edu.unc.ceccr.chembench.persistence.HibernateUtil;
+import edu.unc.ceccr.chembench.persistence.Predictor;
+import edu.unc.ceccr.chembench.persistence.User;
+import edu.unc.ceccr.chembench.utilities.PopulateDataObjects;
+import org.hibernate.Session;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class MyBenchAction extends ActionSupport {
+    private List<?> data;
+
+    public String getDatasets() throws SQLException, ClassNotFoundException {
+        User user = User.getCurrentUser();
+        Session session = HibernateUtil.getSession();
+        List<Dataset> datasets = Lists.newArrayList();
+        if (user != null) {
+            // return user's datasets and public datasets
+            datasets.addAll(PopulateDataObjects.populateDataset(user.getUserName(), Constants.CONTINUOUS, true, session));
+            datasets.addAll(PopulateDataObjects.populateDataset(user.getUserName(), Constants.CATEGORY, true, session));
+        } else {
+            // return public datasets only
+            datasets.addAll(PopulateDataObjects.populateDataset("", Constants.CONTINUOUS, true, session));
+            datasets.addAll(PopulateDataObjects.populateDataset("", Constants.CATEGORY, true, session));
+        }
+        this.data = datasets;
+        session.close();
+        return SUCCESS;
+    }
+
+    public String getModels() throws SQLException, ClassNotFoundException {
+        User user = User.getCurrentUser();
+        Session session = HibernateUtil.getSession();
+        List<Predictor> predictors = Lists.newArrayList();
+        if (user != null) {
+            // return user's models and public models
+            predictors.addAll(PopulateDataObjects.populatePredictors(user.getUserName(), true, true, session));
+        } else {
+            // return public models only
+            predictors.addAll(PopulateDataObjects.populatePredictors("", true, true, session));
+        }
+        this.data = predictors;
+        session.close();
+        return SUCCESS;
+    }
+
+    public List<?> getData() {
+        return data;
+    }
+}
