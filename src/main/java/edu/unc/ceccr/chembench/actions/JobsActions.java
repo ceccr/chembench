602d022058df896d0d3f4bc8be506dc4cd91776c
Ian Kim <iansjk@gmail.com>
2015-05-07 00:12:29 -0400

    Remove duplicated object fetches

--- src/main/java/edu/unc/ceccr/chembench/actions/JobsActions.java
+++ src/main/java/edu/unc/ceccr/chembench/actions/JobsActions.java
@@ -4,40 +4,28 @@ import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 import edu.unc.ceccr.chembench.global.Constants;
 import edu.unc.ceccr.chembench.jobs.CentralDogma;
-import edu.unc.ceccr.chembench.persistence.*;
-import edu.unc.ceccr.chembench.utilities.PopulateDataObjects;
+import edu.unc.ceccr.chembench.persistence.HibernateUtil;
+import edu.unc.ceccr.chembench.persistence.Job;
+import edu.unc.ceccr.chembench.persistence.User;
 import edu.unc.ceccr.chembench.utilities.Utility;
 import org.apache.log4j.Logger;
 import org.hibernate.Session;
 
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
-// struts2
 
 public class JobsActions extends ActionSupport {
-
-    /**
-     *
-     */
-    private static final long serialVersionUID = 42L;
-
     private static Logger logger = Logger.getLogger(JobsActions.class.getName());
+
     // ====== variables used for displaying the JSP =====//
     private User user;
     private boolean adminUser;
     // ====== variables used to hold the queue and finished jobs information
-    // =====//
-    private List<Dataset> userDatasets;
-    private List<Predictor> userPredictors;
-    private List<Prediction> userPredictions;
     private List<Job> incomingJobs;
     private List<Job> lsfJobs;
     private List<Job> localJobs;
     private List<Job> errorJobs;
 
     public String loadPage() throws Exception {
-
         String result = SUCCESS;
 
         // check that the user is logged in
@@ -66,106 +54,6 @@ public class JobsActions extends ActionSupport {
         // set up any values that need to be populated onto the page
         // (dropdowns, lists, display stuff)
         Session session = HibernateUtil.getSession();
-
-        // get datasets
-        if (user.getShowPublicDatasets().equals(Constants.ALL)) {
-            // get the user's datasets and all public ones
-            userDatasets = PopulateDataObjects.populateDataset(user.getUserName(), Constants.CONTINUOUS, true, session);
-            userDatasets
-                    .addAll(PopulateDataObjects.populateDataset(user.getUserName(), Constants.CATEGORY, true, session));
-            userDatasets.addAll(PopulateDataObjects
-                    .populateDataset(user.getUserName(), Constants.PREDICTION, true, session));
-        } else if (user.getShowPublicDatasets().equals(Constants.NONE)) {
-            // just get the user's datasets
-            userDatasets =
-                    PopulateDataObjects.populateDataset(user.getUserName(), Constants.CONTINUOUS, false, session);
-            userDatasets.addAll(PopulateDataObjects
-                    .populateDataset(user.getUserName(), Constants.CATEGORY, false, session));
-            userDatasets.addAll(PopulateDataObjects
-                    .populateDataset(user.getUserName(), Constants.PREDICTION, false, session));
-        } else if (user.getShowPublicDatasets().equals(Constants.SOME)) {
-            // get all the datasets and filter out all the public ones that
-            // aren't "show by default"
-            userDatasets = PopulateDataObjects.populateDataset(user.getUserName(), Constants.CONTINUOUS, true, session);
-            userDatasets
-                    .addAll(PopulateDataObjects.populateDataset(user.getUserName(), Constants.CATEGORY, true, session));
-            userDatasets.addAll(PopulateDataObjects
-                    .populateDataset(user.getUserName(), Constants.PREDICTION, true, session));
-
-            for (int i = 0; i < userDatasets.size(); i++) {
-                String s = userDatasets.get(i).getShowByDefault();
-                if (s != null && s.equals(Constants.NO)) {
-                    userDatasets.remove(i);
-                    i--;
-                }
-            }
-        }
-        if (userDatasets != null) {
-            // Collections.sort(userDatasets, new Comparator<Dataset>() {
-            // public int compare(Dataset d1, Dataset d2) {
-            // return
-            // d1.getName().toLowerCase().compareTo(d2.getName().toLowerCase());
-            // }});
-
-            Collections.sort(userDatasets, new Comparator<Dataset>() {
-                public int compare(Dataset d1, Dataset d2) {
-                    return d2.getCreatedTime().compareTo(d1.getCreatedTime());
-                }
-            });
-
-            for (int i = 0; i < userDatasets.size(); i++) {
-                if (userDatasets.get(i).getJobCompleted() == null || userDatasets.get(i).getJobCompleted()
-                        .equals(Constants.NO)) {
-                    userDatasets.remove(i);
-                    i--;
-                }
-            }
-        }
-        // get predictors
-        if (user.getShowPublicPredictors().equals(Constants.ALL)) {
-            // get the user's predictors and all public ones
-            userPredictors = PopulateDataObjects.populatePredictors(user.getUserName(), true, true, session);
-        } else {
-            // just get the user's predictors
-            userPredictors = PopulateDataObjects.populatePredictors(user.getUserName(), false, true, session);
-        }
-        if (userPredictors != null) {
-            // Collections.sort(userPredictors, new Comparator<Predictor>() {
-            // public int compare(Predictor p1, Predictor p2) {
-            // return
-            // p1.getName().toLowerCase().compareTo(p2.getName().toLowerCase());
-            // }});
-            Collections.sort(userPredictors, new Comparator<Predictor>() {
-                public int compare(Predictor p1, Predictor p2) {
-                    return p2.getDateCreated().compareTo(p1.getDateCreated());
-                }
-            });
-        }
-
-        // get predictions
-        userPredictions = PopulateDataObjects.populatePredictions(user.getUserName(), false, session);
-        if (userPredictions != null) {
-            // Collections.sort(userPredictions, new Comparator<Prediction>()
-            // {
-            // public int compare(Prediction p1, Prediction p2) {
-            // return
-            // p1.getName().toLowerCase().compareTo(p2.getName().toLowerCase());
-            // }});
-
-            Collections.sort(userPredictions, new Comparator<Prediction>() {
-                public int compare(Prediction p1, Prediction p2) {
-                    return p2.getDateCreated().compareTo(p1.getDateCreated());
-                }
-            });
-
-            for (int i = 0; i < userPredictions.size(); i++) {
-                if (userPredictions.get(i).getJobCompleted() == null || userPredictions.get(i).getJobCompleted()
-                        .equals(Constants.NO)) {
-                    userPredictions.remove(i);
-                    i--;
-                }
-            }
-        }
 
         // get local jobs
         localJobs = CentralDogma.getInstance().localJobs.getReadOnlyCopy();
@@ -258,30 +146,6 @@ public class JobsActions extends ActionSupport {
 
     public void setAdminUser(boolean adminUser) {
         this.adminUser = adminUser;
-    }
-
-    public List<Dataset> getUserDatasets() {
-        return userDatasets;
-    }
-
-    public void setUserDatasets(List<Dataset> userDatasets) {
-        this.userDatasets = userDatasets;
-    }
-
-    public List<Predictor> getUserPredictors() {
-        return userPredictors;
-    }
-
-    public void setUserPredictors(List<Predictor> userPredictors) {
-        this.userPredictors = userPredictors;
-    }
-
-    public List<Prediction> getUserPredictions() {
-        return userPredictions;
-    }
-
-    public void setUserPredictions(List<Prediction> userPredictions) {
-        this.userPredictions = userPredictions;
     }
 
     public List<Job> getIncomingJobs() {
