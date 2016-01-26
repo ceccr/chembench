d18ed6b8e545f00d2899228c9fd583ea2ecede89
Ian Kim <iansjk@gmail.com>
2016-01-25 23:07:56 -0500

    Clean up servlet up/down routines

--- src/main/java/edu/unc/ceccr/chembench/global/ChembenchServletContextListener.java
+++ src/main/java/edu/unc/ceccr/chembench/global/ChembenchServletContextListener.java
@@ -1,6 +1,5 @@
 package edu.unc.ceccr.chembench.global;
 
-import com.mysql.jdbc.AbandonedConnectionCleanupThread;
 import edu.unc.ceccr.chembench.jobs.CentralDogma;
 import edu.unc.ceccr.chembench.utilities.Utility;
 import org.apache.log4j.Logger;
@@ -9,10 +8,6 @@ import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import java.io.IOException;
 import java.nio.file.Paths;
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.Enumeration;
 import java.util.Set;
 
 public class ChembenchServletContextListener implements ServletContextListener {
@@ -25,17 +20,17 @@ public class ChembenchServletContextListener implements ServletContextListener {
         try {
             ENV_CHEMBENCH_HOME = System.getenv("CHEMBENCH_HOME");
         } catch (SecurityException e) {
-            logger.error("Couldn't read $CHEMBENCH_HOME environment variable", e);
+            throw new RuntimeException("Couldn't read $CHEMBENCH_HOME environment variable", e);
         }
         if (ENV_CHEMBENCH_HOME == null) {
-            logger.error("Environment variable $CHEMBENCH_HOME doesn't exist");
+            throw new RuntimeException("Environment variable $CHEMBENCH_HOME doesn't exist");
         }
 
         String configFilePath = Paths.get(ENV_CHEMBENCH_HOME, "config", "systemConfig.xml").toString();
         try {
             Utility.readBuildDateAndSystemConfig(configFilePath);
         } catch (IOException e) {
-            logger.error("Couldn't parse system config", e);
+            throw new RuntimeException("Couldn't parse system config", e);
         }
 
         // start up the job queues
@@ -44,25 +39,9 @@ public class ChembenchServletContextListener implements ServletContextListener {
 
     @Override
     public void contextDestroyed(ServletContextEvent servletContextEvent) {
-        try {
-            AbandonedConnectionCleanupThread.shutdown();
-        } catch (InterruptedException e) {
-            logger.error("Shutdown of MySQL abandoned connection cleanup thread failed", e);
-        }
-
         Set<Thread> jobThreads = CentralDogma.getInstance().getThreads();
         for (Thread t : jobThreads) {
-            t.stop();
-        }
-
-        Enumeration<Driver> drivers = DriverManager.getDrivers();
-        while (drivers.hasMoreElements()) {
-            Driver driver = drivers.nextElement();
-            try {
-                DriverManager.deregisterDriver(driver);
-            } catch (SQLException e) {
-                logger.error("Driver deregistration failed", e);
-            }
+            t.interrupt();
         }
     }
 }
