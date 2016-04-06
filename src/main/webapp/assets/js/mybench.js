e26b204ad6f2db6fef5770fc4d4b4995b8f0a901
Ian Kim <iansjk@gmail.com>
2016-04-06 18:02:45 -0400

    Fix load-tab-by-urlhash code
    Missing " " around the href attr selector.

--- src/main/webapp/assets/js/mybench.js
+++ src/main/webapp/assets/js/mybench.js
@@ -33,7 +33,7 @@
         // if given a hash, activate that tab on page load
         var url = document.location.toString();
         if (url.match('#')) {
-            $('.nav-tabs a[href=#' + url.split('#')[1] + ']').tab('show');
+            $('.nav-tabs a[href="#' + url.split('#')[1] + '"]').tab('show');
         }
 
         // change page hash when a tab is clicked
