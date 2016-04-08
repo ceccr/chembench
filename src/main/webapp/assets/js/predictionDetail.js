871a11ba2fa969a3870ebb9d11132ec0f47cb328
Ian Kim <iansjk@gmail.com>
2016-04-08 15:16:57 -0400

    Rename #predictions to #prediction-values
    The #predictions id conflicted with the mybench predictions list table.

--- src/main/webapp/assets/js/predictionDetail.js
+++ src/main/webapp/assets/js/predictionDetail.js
@@ -5,11 +5,11 @@
         $('.nav-list li').removeClass('active');
         $('#nav-button-prediction').addClass('active');
         $('.compound-structure').popover(Chembench.POPOVER_CONFIG);
-        var predictions = $('#predictions');
-        predictions.DataTable($.extend({
+        var predictionValues = $('#prediction-values');
+        predictionValues.DataTable($.extend({
             'order': [[0, 'asc']],
             'scrollX': true
         }, Chembench.DATATABLE_OPTIONS));
-        predictions.show().parents('.dataTables_scrollBody').doubleScroll();
+        predictionValues.show().parents('.dataTables_scrollBody').doubleScroll();
     });
 })();
