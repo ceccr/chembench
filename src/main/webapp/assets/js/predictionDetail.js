afce9a93c5d281f90858e4df1ee88dc585a1c3da
Ian Kim <iansjk@gmail.com>
2016-04-29 18:41:57 -0400

    Fix doubleScroll call on prediction values tbl

--- src/main/webapp/assets/js/predictionDetail.js
+++ src/main/webapp/assets/js/predictionDetail.js
@@ -5,11 +5,13 @@
         $('.nav-list li').removeClass('active');
         $('#nav-button-prediction').addClass('active');
         $('.compound-structure').popover(Chembench.POPOVER_CONFIG);
-        var predictionValues = $('#prediction-values');
-        predictionValues.DataTable($.extend({
+
+        // XXX draw callback must be declared _before_ DataTable() is called
+        $('#prediction-values').on('draw.dt', function() {
+            $(this).closest('.dataTables_scroll').doubleScroll();
+        }).DataTable($.extend({
             'order': [[0, 'asc']],
             'scrollX': true
         }, Chembench.DATATABLE_OPTIONS));
-        predictionValues.show().parents('.dataTables_scrollBody').doubleScroll();
     });
 })();
