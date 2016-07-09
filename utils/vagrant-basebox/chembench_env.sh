2d26967628431fd22a563b57ce02e8eba7aaffff
Ian Kim <iansjk@gmail.com>
2016-07-08 21:51:16 -0400

    Remove old basebox-producing Vagrant files

--- utils/vagrant-basebox/chembench_env.sh
+++
@@ -1,6 +0,0 @@
-# Chembench environment variables
-export CHEMBENCH_HOME=/opt/chembench
-export PATH=$PATH:$CHEMBENCH_HOME/bin:$CHEMBENCH_HOME/jchem/bin
-export CHEMAXON_LICENSE_URL=$CHEMBENCH_HOME/licenses/jchem.cxl
-export DRGX_LICENSEDATA=$CHEMBENCH_HOME/licenses/dragon.txt
-
