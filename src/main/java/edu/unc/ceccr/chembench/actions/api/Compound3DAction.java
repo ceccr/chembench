f2c7209de857751fa52f53496ab1782e228367f8
Ian Kim <iansjk@gmail.com>
2016-07-29 18:14:13 -0400

    Adapt to support ChemDoodle instead of Marvin
    So MarvinView relies on Java applets, which due to Chrome's deprecating NPAPI
    no longer work on Chrome (our audience's most used browser) and applets are
    generally a pain to deal with anyway. ChemDoodle works via WebGL and js which
    is a lot easier to deal with and, most importantly, supports Chrome.

--- src/main/java/edu/unc/ceccr/chembench/actions/api/Compound3DAction.java
+++ src/main/java/edu/unc/ceccr/chembench/actions/api/Compound3DAction.java
@@ -1,39 +1,34 @@
 package edu.unc.ceccr.chembench.actions.api;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.opensymphony.xwork2.ActionSupport;
-import edu.unc.ceccr.chembench.global.Constants;
 import edu.unc.ceccr.chembench.persistence.Dataset;
 import edu.unc.ceccr.chembench.persistence.DatasetRepository;
 import edu.unc.ceccr.chembench.persistence.User;
 import edu.unc.ceccr.chembench.utilities.RunExternalProgram;
-import org.apache.http.client.utils.URIBuilder;
 import org.apache.struts2.interceptor.ServletResponseAware;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.URISyntaxException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 /**
- * Handles requests for 3D .mol files for use with the Marvin compound visualization applet.
+ * Handles requests for 3D .mol files for use in 3D compound visualization.
  */
 public class Compound3DAction extends ActionSupport implements ServletResponseAware {
     private static final Logger logger = LoggerFactory.getLogger(Compound3DAction.class);
-    private static final int APPLET_WIDTH = 350;
-    private static final int APPLET_HEIGHT = 350;
 
     private final DatasetRepository datasetRepository;
     private User user = User.getCurrentUser();
 
     private String compoundName;
     private Long datasetId;
-    private MarvinApplet applet;
     private HttpServletResponse response;
 
     @Autowired
@@ -42,7 +37,7 @@ public class Compound3DAction extends ActionSupport implements ServletResponseAw
     }
 
     /**
-     * Return a json response containing applet width/height and a url to the 3D molfile of the requested compound.
+     * Return a plaintext response containing the 3D .mol file of the requested compound.
      *
      * @return result string
      */
@@ -67,33 +62,19 @@ public class Compound3DAction extends ActionSupport implements ServletResponseAw
         if (!Files.exists(molFilePath)) {
             convert2Dto3D(sdfFilePath, molFilePath);
         }
-        applet = new MarvinApplet();
-        applet.height = APPLET_HEIGHT;
-        applet.width = APPLET_WIDTH;
-        try {
-            URIBuilder uriBuilder = new URIBuilder("/fileServlet");
-            uriBuilder.addParameter("compoundId", compoundName);
-            uriBuilder.addParameter("jobType", Constants.DATASET);
-            uriBuilder.addParameter("file", "mol");
-            uriBuilder.addParameter("datasetName", dataset.getName());
-            uriBuilder.addParameter("userName", dataset.getUserName());
-            applet.url = uriBuilder.build().toString();
-        } catch (URISyntaxException e) {
-            logger.error("URL building failed", e);
-            return ERROR;
-        }
-
-        // the struts2-json-plugin doesn't let us disable html escaping for the url,
-        // so we write to the response manually and return NONE to skip result processing
-        try {
-            Gson gson = new GsonBuilder().disableHtmlEscaping().create();
-            response.getWriter().write(gson.toJson(applet));
-            response.setContentType("application/json");
+        try (BufferedReader reader = Files.newBufferedReader(molFilePath, StandardCharsets.UTF_8)){
             response.setCharacterEncoding("UTF-8");
+            PrintWriter writer = response.getWriter();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                writer.write(line);
+                writer.write('\n');
+            }
         } catch (IOException e) {
             logger.error("Couldn't write to response", e);
             return ERROR;
         }
+        // we've already written directly to the response, so return NONE to skip result processing
         return NONE;
     }
 
@@ -123,49 +104,8 @@ public class Compound3DAction extends ActionSupport implements ServletResponseAw
         this.datasetId = datasetId;
     }
 
-    public MarvinApplet getApplet() {
-        return applet;
-    }
-
-    public void setApplet(MarvinApplet applet) {
-        this.applet = applet;
-    }
-
     @Override
     public void setServletResponse(HttpServletResponse httpServletResponse) {
         this.response = httpServletResponse;
-    }
-
-    /**
-     * Container class for Marvin applet parameters.
-     */
-    private static class MarvinApplet {
-        private int width;
-        private int height;
-        private String url;
-
-        public int getWidth() {
-            return width;
-        }
-
-        public void setWidth(int width) {
-            this.width = width;
-        }
-
-        public int getHeight() {
-            return height;
-        }
-
-        public void setHeight(int height) {
-            this.height = height;
-        }
-
-        public String getUrl() {
-            return url;
-        }
-
-        public void setUrl(String url) {
-            this.url = url;
-        }
     }
 }
