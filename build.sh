d56bd8176ce2f5352b4adf55ce22e7a28ec9fa28
Alexander Tropsha <ceccr@live.unc.edu>
2016-08-02 16:11:13 -0400

    Adjust build script for new versions

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ mvn clean compile war:exploded
 chmod -R g+rwX target
 sudo /etc/init.d/tomcat6 stop
 rm -rf /CHEMBENCH/ROOT/*
-cp -r target/chembench-1.0-SNAPSHOT/* /CHEMBENCH/ROOT
+cp -r target/chembench*/* /CHEMBENCH/ROOT
 sudo /etc/init.d/tomcat6 start
 
