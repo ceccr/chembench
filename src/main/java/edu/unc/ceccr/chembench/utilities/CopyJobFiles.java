501eb614f00cbc94ddeae6b469ac9ad271c05f4e
wlam0001 <wlam0001@live.unc.edu>
2017-05-19 15:19:47 -0400

    remove excessive import for CopyJobFiles.java

--- src/main/java/edu/unc/ceccr/chembench/utilities/CopyJobFiles.java
+++ src/main/java/edu/unc/ceccr/chembench/utilities/CopyJobFiles.java
@@ -15,8 +15,6 @@ import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
