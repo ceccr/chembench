ebf31bfb565c613005b2aa67ff7769876ae06930
kamisch <chengtwv2@gmail.com>
2018-04-10 13:41:46 -0400

    added loggers to dragon7

--- src/main/java/edu/unc/ceccr/chembench/workflows/descriptors/DescriptorDragon7.java
+++ src/main/java/edu/unc/ceccr/chembench/workflows/descriptors/DescriptorDragon7.java
@@ -30,17 +30,17 @@ public class DescriptorDragon7 extends DescriptorCommonDragon implements Descrip
     @Override
     public void generateDescriptors(String sdfFile, String outFile) throws DescriptorGenerationException {
         try {
-            logger.debug(sdfFile);
+            logger.debug("dragon7bug" + sdfFile);
             String scriptFilePath = Paths.get(Constants.CECCR_BASE_PATH, Constants.DRAGON7_SCRIPT_PATH).toString();
             String execstr = Utility.SPACE_JOINER.join(new String[]{"dragon7",
                     "-s", scriptFilePath,
                     "<", sdfFile
             });
-            logger.debug(execstr);
+            logger.debug("dragon7bug" + execstr);
             Path sdfFilePath = Paths.get(sdfFile);
             Path descriptorsDirPath = sdfFilePath.getParent().resolve("Descriptors");
             RunExternalProgram.runCommandAndLogOutput(execstr, descriptorsDirPath.toString() + "/", "dragon7");
-            logger.debug(execstr);
+            logger.debug("dragon7bug" + execstr);
             try {
                 Files.move(descriptorsDirPath.resolve("dragon7.out"), Paths.get(outFile + getFileEnding()), StandardCopyOption.REPLACE_EXISTING);
             } catch (IOException e) {
@@ -50,7 +50,7 @@ public class DescriptorDragon7 extends DescriptorCommonDragon implements Descrip
             logger.error("Dragon 7 descriptor generation failed; not adding to available descriptors", e);
         }
     }
-
+    
     @Override
     public String checkDescriptors(String outputFile) throws Exception {
         File dragon7File = new File(outputFile + getFileEnding());
